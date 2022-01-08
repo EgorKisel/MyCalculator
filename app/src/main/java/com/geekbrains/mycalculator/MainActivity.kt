a7b433f352d36a6a386c5a4ff7abd6b4a0cfb069
Egor Kisel <kisel1989@mail.ru>
2022-01-08 15:26:21 +0300

    Применил действия к кнопкам

--- app/src/main/java/com/geekbrains/mycalculator/MainActivity.kt
+++ app/src/main/java/com/geekbrains/mycalculator/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.geekbrains.mycalculator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.*
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    private var arg1: Int? = null
+    private var arg2: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
+
+        val button = findViewById<Button>(R.id.button)
+        val editText1 = findViewById<TextView>(R.id.editText1)
+        val editText2 = findViewById<TextView>(R.id.editText2)
+        val textView = findViewById<TextView>(R.id.textView)
+
+
+
+        button.setOnClickListener {
+            try {
+                arg1 = Integer.valueOf(editText1.text.toString())
+                arg2 = Integer.valueOf(editText2.text.toString())
+                if (arg1?.equals(arg2) == true) {
+                    textView.text = "Равно!"
+                } else {
+                    textView.text = "Не равно!"
+                }
+            } catch (e: Exception) {
+                textView.text = "Введите число"
+            }
+        }
+
     }
 }
+
+
