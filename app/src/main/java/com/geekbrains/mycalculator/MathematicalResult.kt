8aa86c4a98c89eaab12c3eb94a0550a8b663fa82
Egor Kisel <kisel1989@mail.ru>
2022-10-30 07:27:18 +0300

    добавил горизонтальную разметку

--- app/src/main/java/com/geekbrains/mycalculator/MathematicalResult.kt
+++ app/src/main/java/com/geekbrains/mycalculator/MathematicalResult.kt
@@ -3,7 +3,7 @@ package com.geekbrains.mycalculator
 import android.os.Parcel
 import android.os.Parcelable
 
-public class MatchematicResult() : Parcelable {
+class MathematicalResult() : Parcelable {
     constructor(parcel: Parcel) : this() {
     }
 
@@ -15,12 +15,12 @@ public class MatchematicResult() : Parcelable {
         return 0
     }
 
-    companion object CREATOR : Parcelable.Creator<MatchematicResult> {
-        override fun createFromParcel(parcel: Parcel): MatchematicResult {
-            return MatchematicResult(parcel)
+    companion object CREATOR : Parcelable.Creator<MathematicalResult> {
+        override fun createFromParcel(parcel: Parcel): MathematicalResult {
+            return MathematicalResult(parcel)
         }
 
-        override fun newArray(size: Int): Array<MatchematicResult?> {
+        override fun newArray(size: Int): Array<MathematicalResult?> {
             return arrayOfNulls(size)
         }
     }
